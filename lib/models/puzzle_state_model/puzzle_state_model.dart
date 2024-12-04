f3b57c678c13e61ebc31e268e86da5d6b68eeef6
Kyria-Ashraf <keirayaashraf@gmail.com>
2024-12-04 03:03:45 +0200

    add manhattan distance function

--- lib/models/puzzle_state_model/puzzle_state_model.dart
+++ lib/models/puzzle_state_model/puzzle_state_model.dart
@@ -17,6 +17,20 @@ class PuzzleState {
     return count;
   }
 
+  int manhattanDistance(List<List<int>> goalState) {
+    int distance = 0;
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] != 0) {
+          int goalRow = (board[i][j] - 1) ~/ 3;
+          int goalCol = (board[i][j] - 1) % 3;
+          distance += (i - goalRow).abs() + (j - goalCol).abs();
+        }
+      }
+    }
+    return distance;
+  }
+
   bool isGoal(List<List<int>> goalState) {
     return board.toString() == goalState.toString();
   }
